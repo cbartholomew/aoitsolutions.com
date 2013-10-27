ac7835c6dab88c352d7e9a84c1739738bfb1cf6d
Christopher Bartholomew <cbartholomew@gmail.com>
2013-10-26 20:52:05 -0400

    commented out absolute positioning due to bug whereby actions all place on top of one another

--- SIC2013/agendaboard/View/tweet.php
+++ SIC2013/agendaboard/View/tweet.php
@@ -4,7 +4,7 @@
 			<b><a href="https://twitter.com/#TWITTER_HANDLE#" target="_blank">#TWITTER_HANDLE#</a></b>
 		</div>
 		<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 tweet twitterActions'>
-			<a href="https://twitter.com/intent/tweet?in_reply_to=#TWITTER_TWEET_ID#"  target="_blank"><i class='glyphicon glyphicon-share-alt'></i></a>&nbsp;<a href="https://twitter.com/intent/retweet?tweet_id=#TWITTER_TWEET_ID#"   target="_blank"><i class='glyphicon glyphicon-retweet'></i></a>&nbsp;<a href="https://twitter.com/intent/favorite?tweet_id=#TWITTER_TWEET_ID#"  target="_blank"><i class='glyphicon glyphicon-heart-empty'></i></a>
+			<a  href="https://twitter.com/intent/tweet?in_reply_to=#TWITTER_TWEET_ID#" class="#TWITTER_TWEET_ID#" target="_blank"><i class='glyphicon glyphicon-share-alt'></i></a>&nbsp;<a href="https://twitter.com/intent/retweet?tweet_id=#TWITTER_TWEET_ID#"  class="#TWITTER_TWEET_ID#" target="_blank"><i class='glyphicon glyphicon-retweet'></i></a>&nbsp;<a href="https://twitter.com/intent/favorite?tweet_id=#TWITTER_TWEET_ID#" class="#TWITTER_TWEET_ID#" target="_blank"><i class='glyphicon glyphicon-heart-empty'></i></a>
 		</div>
 	</div>								
 	<div class='row'>
