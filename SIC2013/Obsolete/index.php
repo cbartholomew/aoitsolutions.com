217e9110b5621d0c82b9aaba82c369c7ef11cd87
Christopher Bartholomew <cbartholomew@gmail.com>
2013-12-06 15:19:31 -0500

    updates to index to remove header info

--- SIC2013/Obsolete/index.php
+++ SIC2013/Obsolete/index.php
@@ -1,49 +1,109 @@
 <?php
+	/**
+	 * Index.php
+	 *
+	 * Christopher Bartholomew
+	 * cbartholomew@gmail.com
+	 * 
+	 * Main index controller
+	 */
 	require("includes/config.php");
 	require("Model/ViewManager.php");
-	require("Controller/Category.php");
+	require("Model/TwitterSearchAPI.php");
 	require("Controller/Session.php");
-	require("Controller/Speaker.php");
-	require("Controller/Sponsor.php");
-	
+		
+	// get date from query string - if it's set manually, it will over ride current date 
+	$dayNo =  (isset($_GET["dayno"])) ? $_GET["dayno"] : GetDayNo(getdate());
+		
 	// make new view manager
-	$viewManager = new ViewManager();
-	
-	// get business objects by site_id and channel id
-	// OBSOLETE - JSON Direct URL covers this
-	$sessions 	= Session::get(2,16);
-	$speakers 	= Speaker::get(2,17);
-	$sponsors   = Sponsor::get(2,18);
-	$speakerByEntryId = Speaker::getByEntryId(600);
-
+	$viewManager = new ViewManager("View/");
 	
 	// get the agenda board configuration
-	$agenda_json = file_get_contents('Configuration/Agenda.json');
-	$agenda    = json_decode($agenda_json,true);
+	$agenda_uri  = (IS_PROD) ? JSON_AGENDA_PROD_URI : JSON_AGENDA_URI;
+		
+	$agenda_json = file_get_contents($agenda_uri);
 	
+	// make json object
+	$agenda      = json_decode($agenda_json,true);
 	
+	// run sort on rooms by display order
+	$agenda["Rooms"] = SortObjectByProperty($agenda["Rooms"],"cmpDisplayOrder");
+	
+	// get the sessions via json
+	$session_uri  = (IS_PROD) ? JSON_SESSION_PROD_URI : JSON_SESSION_TEST_URI;
+
+	/*	
+	// time difference
+	$requestTimeInMinutes = 0;
+	
+	// force pacific standard time
+	date_default_timezone_set("America/Los_Angeles");
+	
+	// store session request in minutes
+	$sessionDate = getdate();
+	
+	$sessionDate = $sessionDate["minutes"];
+	
+	// create variable to store the difference
+	$lastRequestDTTM = 0;
+	
+	// if the session's last request is set and not empty
+
+	if(isset($_SESSION["LASTDTTM"]) && !empty($_SESSION["LASTDTTM"]))
+	{
+		// .. then get the last request in minutes
+		$lastRequestDTTM = $_SESSION["LASTDTTM"];
+		
+		// process the difference 
+		$requestTimeInMinutes = $sessionDate - $lastRequestDTTM;
+	}
+	*/
+	/*
+	// read from file, but make a new request for the json when last request is > two minutes
+	if($requestTimeInMinutes > 3)
+	{
+		// look up and save new file
+		file_put_contents ("Configuration/mysessions.json",file_get_contents($session_uri));
+		
+		// set the file uri to the new file created
+		$session_uri  = "Configuration/mysessions.json";
+		
+		// set the last update time in minutes to session
+		$lastRequestInMinutes = getdate();
+		// get just minutes
+		$lastRequestInMinutes = $lastRequestInMinutes["minutes"];
+		// load last request
+		$_SESSION["LASTDTTM"] = $lastRequestInMinutes;	
+	}
+	else
+	{		
+		// send local config instead
+		$session_uri  = "Configuration/mysessions.json";
+	}
+	*/
+	
+	// get file contents
+	$session_json = file_get_contents($session_uri);
+	// decode the contents
+	$sessions     = json_decode($session_json, true);
+	
+	$session 	  = new Session(count($sessions["Sessions"]));
+
+	// get current header
+	$headerImage = GetHeader($dayNo);
 	
 	// pass the arguments to the view
 	$arguments = array(
-				"sessions"   		=> $sessions,
-				"speakers" 	 		=> $speakers,
-				"sponsors"			=> $sponsors,
-				"speakerByEntryId"  => $speakerByEntryId,
-				"agenda"			=> $agenda
+				"sessions"   		=> $sessions["Sessions"],
+				"agenda"			=> $agenda,
+				"headerImage"		=> $headerImage,
+				"day"				=> $dayNo,
+				"session"			=> $session,
+				"viewManager"		=> $viewManager				
 			);
 	// render view
 	$viewManager->renderView("agenda",$arguments);
-	// Template used for testing until they enter the data
-	// for($i=0,$n=$agenda["RoomCount"];$i<$n;$i++)
-	// {			
-	// 	$backgroundCls = GetBackgroundCSS($room["Number"]);
-	// 	
-	// 	$sessionHtml = "TBD";
-	// 							
-	// 	// extract the room
-	// 	$room = $agenda["Rooms"][$i];																
-	// 	print("<td class='". $columnCls . " " . $backgroundCls . " sessionColumn " . 
-	// 		   str_replace(":","_",$s["Start"]) . "_" . $room["Number"] ."'>" .
-	// 		   $sessionHtml . "</td>");
-	// }
+	
+	// test agenda
+	//$viewManager->renderView("agendatest",$arguments);	
 ?>
--- SIC2013/agendaboard/index.php
+++ SIC2013/agendaboard/index.php
@@ -31,56 +31,6 @@
 	
 	// get the sessions via json
 	$session_uri  = (IS_PROD) ? JSON_SESSION_PROD_URI : JSON_SESSION_TEST_URI;
-
-	/*	
-	// time difference
-	$requestTimeInMinutes = 0;
-	
-	// force pacific standard time
-	date_default_timezone_set("America/Los_Angeles");
-	
-	// store session request in minutes
-	$sessionDate = getdate();
-	
-	$sessionDate = $sessionDate["minutes"];
-	
-	// create variable to store the difference
-	$lastRequestDTTM = 0;
-	
-	// if the session's last request is set and not empty
-
-	if(isset($_SESSION["LASTDTTM"]) && !empty($_SESSION["LASTDTTM"]))
-	{
-		// .. then get the last request in minutes
-		$lastRequestDTTM = $_SESSION["LASTDTTM"];
-		
-		// process the difference 
-		$requestTimeInMinutes = $sessionDate - $lastRequestDTTM;
-	}
-	*/
-	/*
-	// read from file, but make a new request for the json when last request is > two minutes
-	if($requestTimeInMinutes > 3)
-	{
-		// look up and save new file
-		file_put_contents ("Configuration/mysessions.json",file_get_contents($session_uri));
-		
-		// set the file uri to the new file created
-		$session_uri  = "Configuration/mysessions.json";
-		
-		// set the last update time in minutes to session
-		$lastRequestInMinutes = getdate();
-		// get just minutes
-		$lastRequestInMinutes = $lastRequestInMinutes["minutes"];
-		// load last request
-		$_SESSION["LASTDTTM"] = $lastRequestInMinutes;	
-	}
-	else
-	{		
-		// send local config instead
-		$session_uri  = "Configuration/mysessions.json";
-	}
-	*/
 	
 	// get file contents
 	$session_json = file_get_contents($session_uri);
